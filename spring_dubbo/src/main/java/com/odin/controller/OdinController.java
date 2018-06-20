672e52ae1c8122774be693cdd789d459d419901c
xuetao <XUETAO##11>
2018-06-20 15:56:31 +0800

    spring项目搭建

---
+++ spring_dubbo/src/main/java/com/odin/controller/OdinController.java
@@ -0,0 +1,18 @@
+package com.odin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther: ${Odin}
+ * @Date: 2018/6/13 16:51
+ * @Description:
+ */
+@Controller
+@RequestMapping("/web")
+public class OdinController {
+
+
+
+
+}
---
+++ spring_dubbo/src/main/java/com/odin/entity/People.java
@@ -0,0 +1,40 @@
+package com.odin.entity;
+
+/**
+ * @Auther: ${Odin}
+ * @Date: 2018/6/13 16:45
+ * @Description:
+ */
+public class People {
+
+    // 姓名
+    private String name;
+    // 年龄
+    private int age;
+    // 性别
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
